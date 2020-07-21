80198136a1c7c50a2d4d68cf271f90427f21933a
Jeremy May <may.jeremy.r@gmail.com>
2020-07-21 14:38:52 -0700

    Updates

---
+++ precache-manifest.6115dcc0280987e06b18ed191fc193ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fc80100c81731bc116684b80f8d00c",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "845a9c0a78aee0c15611",
+    "url": "/coin-exchange/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2b3d1ead97125129a9c7",
+    "url": "/coin-exchange/static/js/2.8784e415.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coin-exchange/static/js/2.8784e415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845a9c0a78aee0c15611",
+    "url": "/coin-exchange/static/js/main.630ef8da.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coin-exchange/static/media/logo.5d5d9eef.svg"
+  }
+]);
